c2d45fdf5ff32860c85b128d9aa8afe567d1b864
Vengeance00 <vengeance801@gmail.com>
2021-03-17 13:11:20 -0600

    Add files via upload

---
+++ Ejercicio 3 Computed/js/app.js
@@ -0,0 +1,19 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        mensaje: 'ola k ase',
+        contador: 0
+    },
+    computed: {
+        invertido (){
+            return this.mensaje.split("").reverse().join("");
+        },
+        color (){
+            return {
+                'bg-success' : this.contador <= 10,
+                'bg-warning' : this.contador > 10 && this.contador < 20,
+                'bg-danger' : this.contador >= 20
+            }
+        }
+    }
+});
